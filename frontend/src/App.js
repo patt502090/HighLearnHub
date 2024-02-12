425b55deded0a58649dfd81a8f5fd7fc8d15bce9
Farik <6610110223@psu.ac.th>
2024-02-12 22:46:05 +0700

    testcourse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import { Button } from "flowbite-react";
 import Navbar from "./pages/navbar";
+import Course from "./componant/course";
 
 function App() {
   return (
     <>
       <Navbar> </Navbar>
+      <Course/>
+      
       <Outlet />
     </>
   );
