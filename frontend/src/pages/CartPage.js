bfcc2041941699c89cac5d8e5f2b5392209fb2a8
Farik <6610110223@psu.ac.th>
2024-02-21 16:22:33 +0700

    edit func caltotalprice

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import ax from "../conf/ax";
 import conf from "../conf/main";
 import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+
 
 export default function CartPage() {
   const [coursebooked, setCoursebooked] = useState();
@@ -11,7 +14,7 @@ export default function CartPage() {
       try {
         const response = await ax.get(
           conf.apiUrlPrefix +
-            "/courses?populate[bookings][filters][payment_status][$eq]=false&populate=image"
+          "/courses?populate[bookings][filters][payment_status][$eq]=false&populate=image"
         );
         console.log(response);
         const filterDatas = response.data.data.filter(
@@ -32,69 +35,72 @@ export default function CartPage() {
       totalPrice +=
         item.attributes.price * item.attributes.bookings.data.length;
     });
-    return totalPrice;
+    return totalPrice.toLocaleString();;
   };
 
   console.log(coursebooked);
   return (
-    <div className="flex flex-col items-center mt-6">
-      <h className="text-xl font-bold">Mycart</h>
+    <>
+      <Navbar />
       <div className="flex flex-col items-center mt-6">
-        <section className="py-17 bg-gray-100 font-poppins dark:bg-gray-700">
-          <div className="px-4 py-6 mx-auto max-w-7xl lg:py-4 md:px-6">
-            <div>
-              <h2 className="mb-8 text-4xl font-bold dark:text-gray-400">
-                Your Cart
-              </h2>
-              <div className="p-6 mb-8 border bg-gray-50 dark:bg-gray-800 border-gray-800">
-                
-                <div className="py-4 mb-8 border-t border-b border-gray-200 dark:border-gray-700">
-                  {coursebooked?.map((item) => (
-                    <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                      <img
-                        class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                        src={
-                          "http://localhost:1337" +
-                          item.attributes.image.data.attributes.url
-                        }
-                        alt=""
-                      ></img>
-                      <div class="flex flex-col justify-between p-4 leading-normal">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                          {item.attributes.title}
-                        </h5>
-                        <p>{item.attributes.description}</p>
-  
-                        <hr className="mt-6 " />
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-between">
-                          <span>ราคา {item.attributes.price} บาท,</span>
-                          <span>x{item.attributes.bookings.data.length}</span>
-                        </p>
+        <h className="text-xl font-bold">Mycart</h>
+        <div className="flex flex-col items-center mt-6">
+          <section className="py-17 bg-gray-100 font-poppins dark:bg-gray-700">
+            <div className="px-4 py-6 mx-auto max-w-7xl lg:py-4 md:px-6">
+              <div>
+                <h2 className="mb-8 text-4xl font-bold dark:text-gray-400">
+                  Your Cart
+                </h2>
+                <div className="p-6 mb-8 border bg-gray-50 dark:bg-gray-800 border-gray-800">
+
+                  <div className="py-4 mb-8 border-t border-b border-gray-200 dark:border-gray-700">
+                    {coursebooked?.map((item) => (
+                      <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <img
+                          class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                          src={
+                            "http://localhost:1337" +
+                            item.attributes.image.data.attributes.url
+                          }
+                          alt=""
+                        ></img>
+                        <div class="flex flex-col justify-between p-4 leading-normal">
+                          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            {item.attributes.title}
+                          </h5>
+                          <p>{item.attributes.description}</p>
+
+                          <hr className="mt-6 " />
+                          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-between">
+                            <span>ราคา {item.attributes.price} บาท,</span>
+                            <span>x{item.attributes.bookings.data.length}</span>
+                          </p>
+                        </div>
                       </div>
+                    ))}
+                  </div>
+                  <div className="flex flex-wrap justify-between">
+                    <div className="w-full px-4 mb-4 lg:w-1/2 ">
+                      ราคาทั้งหมด : {calculateTotalPrice()}
                     </div>
-                  ))}
-                </div>
-                <div className="flex flex-wrap justify-between">
-                  <div className="w-full px-4 mb-4 lg:w-1/2 ">
-                    ราคาทั้งหมด :
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          
-        </section>
-        <div className="flex items-center justify-between ">
+
+          </section>
+          <div className="flex items-center justify-between ">
             <Link Link to="/payment">
-              <button className="block w-full py-5 px-11 font-bold text-center text-gray-100 uppercase bg-blue-500 rounded-md hover:bg-blue-600"
-               style={{ marginTop: "20px" ,marginBottom:"20px"}}
+              <button className="block w-full py-5 px-11 font-bold text-center text-gray-100 uppercase bg-green-400 rounded-md hover:bg-blue-600"
+                style={{ marginTop: "20px", marginBottom: "20px" }}
               >
-                Checkout
+                ชำระเงิน
               </button>
             </Link>
           </div>
+        </div>
       </div>
-    </div>
+    </>
   );
-  
+
 }
--- frontend/src/pages/PaymentPage.js
+++ frontend/src/pages/PaymentPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useParams, Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 export default function PaymentPage() {
     const [selectedFile, setSelectedFile] = useState(null);
@@ -9,47 +10,50 @@ export default function PaymentPage() {
     };
 
     return (
-        <div className='flex flex-col items-center mt-6'>
-            <h2 className='text-xl font-bold'>PaymentPage</h2>
-            <section className="py-17 bg-gray-100 font-poppins dark:bg-gray-700">
-                <div className="px-4 py-6 mx-auto max-w-7xl lg:py-4 md:px-6">
-                    <div>
-                        <div className="p-6 mb-8 border bg-gray-50 dark:bg-gray-800 border-gray-800 rounded-md">
-                            <div className="flex flex-col md:flex-row items-center justify-center mb-6 -mx-4 md:flex md:mb-8">
-                                <div className="md:w-1/2 p-4">
-                                    <img src="https://ตลับหมึกปริ้นเตอร์.com/sites/2110/files/s/articles/o_1bo96me2d2s61gg01vq1g5kkrk7.jpg" alt="Thai Bank" className="h-48 w-auto mx-auto" />
-                                </div>
-                                <div className="md:w-1/2 p-4">
-                                    <div className="flex flex-col items-center justify-center">
-                                        <p className="text-xl font-semibold text-center mt-2">ธนาคารไทยพาณิชย์</p>
-                                        <p className="text-lg text-center">ชื่อบัญชี: Jone Doe</p>
-                                        <p className="text-lg text-center">หมายเลขบัญชี: 123-4-56789-0</p>
+        <>
+            <Navbar />
+            <div className='flex flex-col items-center mt-6'>
+                <h2 className='text-xl font-bold'>PaymentPage</h2>
+                <section className="py-17 bg-gray-100 font-poppins dark:bg-gray-700">
+                    <div className="px-4 py-6 mx-auto max-w-7xl lg:py-4 md:px-6">
+                        <div>
+                            <div className="p-6 mb-8 border bg-gray-50 dark:bg-gray-800 border-gray-800 rounded-md">
+                                <div className="flex flex-col md:flex-row items-center justify-center mb-6 -mx-4 md:flex md:mb-8">
+                                    <div className="md:w-1/2 p-4">
+                                        <img src="https://ตลับหมึกปริ้นเตอร์.com/sites/2110/files/s/articles/o_1bo96me2d2s61gg01vq1g5kkrk7.jpg" alt="Thai Bank" className="h-48 w-auto mx-auto" />
+                                    </div>
+                                    <div className="md:w-1/2 p-4">
+                                        <div className="flex flex-col items-center justify-center">
+                                            <p className="text-xl font-semibold text-center mt-2">ธนาคารไทยพาณิชย์</p>
+                                            <p className="text-lg text-center">ชื่อบัญชี: Jone Doe</p>
+                                            <p className="text-lg text-center">หมายเลขบัญชี: 123-4-56789-0</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="flex flex-col md:flex-row items-center justify-center mb-6 -mx-4 md:flex md:mb-8">
-                                <div className="md:w-1/2 p-4">
-                                    <img src="https://mpics.mgronline.com/pics/Images/566000003112201.JPEG" alt="Thai Bank" className="h-48 w-auto mx-auto" />
-                                </div>
-                                <div className="md:w-1/2 p-4">
-                                    <div className="flex flex-col items-center justify-center">
-                                        <p className="text-xl font-semibold text-center mt-2">ธนาคารไทยกรุงไทย</p>
-                                        <p className="text-lg text-center">ชื่อบัญชี: Jone Doe</p>
-                                        <p className="text-lg text-center">หมายเลขบัญชี: 123-4-56789-0</p>
+                                <div className="flex flex-col md:flex-row items-center justify-center mb-6 -mx-4 md:flex md:mb-8">
+                                    <div className="md:w-1/2 p-4">
+                                        <img src="https://mpics.mgronline.com/pics/Images/566000003112201.JPEG" alt="Thai Bank" className="h-48 w-auto mx-auto" />
+                                    </div>
+                                    <div className="md:w-1/2 p-4">
+                                        <div className="flex flex-col items-center justify-center">
+                                            <p className="text-xl font-semibold text-center mt-2">ธนาคารไทยกรุงไทย</p>
+                                            <p className="text-lg text-center">ชื่อบัญชี: Jone Doe</p>
+                                            <p className="text-lg text-center">หมายเลขบัญชี: 123-4-56789-0</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </section>
+                <div className="py-4">
+                    <input type="file" onChange={handleFileChange} className="hidden" id="fileInput" />
+                    <label htmlFor="fileInput" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer focus:outline-none focus:shadow-outline mb-4 md:mb-0 md:mr-4">เเนบสลิปรูปภาพ</label>
+                    <Link Link to="/finishpayment">
+                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 md:mb-0 md:ml-4">ยืนยันการชำระเงิน</button>
+                    </Link>
                 </div>
-            </section>
-            <div className="py-4">
-                <input type="file" onChange={handleFileChange} className="hidden" id="fileInput" />
-                <label htmlFor="fileInput" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer focus:outline-none focus:shadow-outline mb-4 md:mb-0 md:mr-4">เเนบสลิปรูปภาพ</label>
-                <Link Link to="/finishpayment">
-                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 md:mb-0 md:ml-4">ยืนยันการชำระเงิน</button>
-                </Link>
             </div>
-        </div>
+        </>
     )
 }
