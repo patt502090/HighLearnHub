28de39f5461c163fdb5f0179998e903c6b702b25
ธุวานนท์ สงธนู <6610110134@psu.ac.th>
2024-02-22 12:41:38 +0700

    fix finishpaymentPage,HistoryPage and Progessbar

--- frontend/src/pages/FinishPaymentPage.js
+++ frontend/src/pages/FinishPaymentPage.js
@@ -1,37 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Progessbar from "../components/Progessbar";
 
 function FinishPaymentPage() {
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowConfirmation(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <Navbar />
       <Progessbar></Progessbar>
-      <body>
-        <div class="flex flex-col justify-center items-center h-[100vh] pt-4">
-          <div class="relative flex flex-col items-center rounded-[10px] border-[1px] border-gray-200 w-[576px] mx-auto p-4 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
-            <div class="flex items-center justify-center rounded-t-3xl p-3 w-full text-lg font-bold">
-              ชำระเงินเสร็จสิ้น
+      <div className="flex flex-col justify-center items-center h-[100vh] pt-4">
+        <div className="relative flex flex-col items-center rounded-[10px] border-[1px] border-gray-200 w-[576px] mx-auto p-4 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
+          <div className="flex items-center justify-center rounded-t-3xl p-3 w-full text-lg font-bold">
+            {showConfirmation ? "รอการตรวจสอบ" : "ชำระเงินเสร็จสิ้น"}
+          </div>
+          {showConfirmation ? (
+            <div className="text-center p-3">
+              <div className="flex items-center justify-center mb-3">
+                <img
+                  src="https://media.istockphoto.com/id/944011350/vector/hourglass-with-transparent-glass.jpg?s=612x612&w=0&k=20&c=Y1K39qKz-od-EFf6AF-vOVawB2ncXIX7baW0Zyetp_8="
+                  alt="รอการตรวจสอบ"
+                  style={{ maxWidth: "350px" }}
+                />
+              </div>
+              <ul style={{ listStyleType: "circle", textAlign: "left" }}>
+                <li>กรุณารอผลการตรวจสอบการชำระเงินภายใน 1 - 2 ชั่วโมง</li>
+                <li>โดยสินค้าจะเข้าสู่บัญชีของท่านภายหลังจากการตรวจสอบความถูกต้องเสร็จสิ้น</li>
+              </ul>
             </div>
-            <img
-              class="h-auto max-w-full rounded-xl"
-              src="https://i.pinimg.com/736x/5d/b3/60/5db360def4d6a542f802d74cc94fe549.jpg"
-              alt=""
-            />
-            <div class="flex items-center justify-center rounded-t-3xl p-3 w-full text-lg font-bold">
+          ) : (
+            <div className="flex items-center justify-center">
+              <img
+                className="h-auto max-w-full rounded-xl"
+                src="https://i.pinimg.com/736x/5d/b3/60/5db360def4d6a542f802d74cc94fe549.jpg"
+                alt=""
+                style={{ marginBottom: "20px" }} 
+              />
+            </div>
+          )}
+          {showConfirmation && (
+            <div className="flex items-center justify-center rounded-t-3xl p-3 w-full text-lg font-bold">
               ขอบคุณที่ใช้บริการ
             </div>
-          </div>
+          )}
         </div>
-        <div class="flex justify-center mt-10 mb-10">
-          <Link to="/mycourse">
-            <button className="py-5 px-11 font-bold text-center text-gray-100 uppercase bg-blue-500 rounded-md hover:bg-blue-600">
-              คอร์สของฉัน
-            </button>
-          </Link>
-        </div>
-      </body>
+      </div>
+      <div className="flex justify-center mt-0 mb-10">
+        <Link to="/mycourse">
+          <button className="py-5 px-11 font-bold text-center text-gray-100 uppercase bg-blue-500 rounded-md hover:bg-blue-600" >
+            คอร์สของฉัน
+          </button>
+        </Link>
+      </div>
     </>
   );
 }
--- frontend/src/pages/HistoryPage.js
+++ frontend/src/pages/HistoryPage.js
@@ -1,94 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ax from "../conf/ax";
+import Navbar from "../components/Navbar";
+import conf from "../conf/main";
 
-function HistoryPage() {
+export default function HistoryPage() {
+  const [courseData, setCourseData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [coursebooked, setCoursebooked] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await ax.get(
+          conf.apiUrlPrefix +
+            "/courses?populate[bookings][filters][payment_status][$eq]=false&populate=image"
+        );
+        console.log(response);
+        const filterDatas = response.data.data.filter(
+          (item) => item.attributes.bookings.data.length !== 0
+        );
+        setCoursebooked(filterDatas);
+      } catch (error) {
+        console.error("Error fetching Data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <html>
-      <head>
-          <link rel="stylesheet" href="https://horizon-tailwind-react-corporate-7s21b54hb-horizon-ui.vercel.app/static/css/main.d7f96858.css" />
-      </head>
-      <body >
-          <div class="flex flex-col justify-center items-center h-[100vh] pt-4">
-              <div class="relative flex flex-col items-center rounded-[10px] border-[1px] border-gray-200 w-[576px] mx-auto p-4 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
-                  <div class="flex items-center justify-between rounded-t-3xl p-3 w-full">
-                      <div class="text-lg font-bold text-navy-700 dark:text-white">
-                          History
-                      </div>
-                      <button class="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-medium text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
-                          See all
-                      </button>
+    <>
+      <Navbar />
+      <div>
+        <head>
+          <link
+            rel="stylesheet"
+            href="https://horizon-tailwind-react-corporate-7s21b54hb-horizon-ui.vercel.app/static/css/main.d7f96858.css"
+          />
+        </head>
+        <div className="flex flex-col justify-center items-center h-[60vh] pt-4">
+          <div className="relative flex flex-col items-center rounded-[10px] border-[1px] border-gray-200 w-[576px] mx-auto p-4 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
+            <div className="flex items-center justify-between rounded-t-3xl p-3 w-full">
+              <div className="text-lg font-bold text-navy-700 dark:text-white">
+                History
+              </div>
+              <button className="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-medium text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
+                See all
+              </button>
+            </div>
+            {coursebooked.map((course) => (
+              <div
+                key={course.id}
+                className="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700"
+              >
+                <div className="flex items-center gap-3">
+                  <div className="flex h-16 w-16 items-center justify-center">
+                    <img
+                      className="object-cover w-full h-[100px] rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                      src={
+                        "http://localhost:1337" +
+                        course.attributes.image.data.attributes.url
+                      }
+                      alt=""
+                    />
                   </div>
-                  <div class="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700">
-                      <div class="flex items-center gap-3">
-                          <div class="flex h-16 w-16 items-center justify-center">
-                              <img
-                                  class="h-full w-full rounded-xl"
-                                  src="https://horizon-tailwind-react-corporate-7s21b54hb-horizon-ui.vercel.app/static/media/Nft1.0fea34cca5aed6cad72b.png"
-                                  alt=""
-                              />
-                          </div>
-                          <div class="flex flex-col">
-                              <h5 class="text-base font-bold text-navy-700 dark:text-white">
-                                  Colorful Heaven
-                              </h5>
-                              <p class="mt-1 text-sm font-normal text-gray-600">
-                                  Mark Benjamin
-                              </p>
-                          </div>
-                      </div> 
-                      <div class="mt-1 flex items-center justify-center text-navy-700 dark:text-white">
-                          <div>
-                              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
-                          </div>
-                          <div class="ml-1 flex items-center text-sm font-bold text-navy-700 dark:text-white">
-                              <p>   </p>
-                              2900<p class="ml-1">Bath</p>
-                          </div>
-                          <div class="ml-2 flex items-center text-sm font-normal text-gray-600 dark:text-white">
-                              <p>5d</p>
-                              <p class="ml-1">ago</p>
-                          </div>
-                      </div>
+                  <div className="flex flex-col">
+                    <h5 className="text-base font-bold text-navy-700 dark:text-white">
+                      {course.attributes.title}
+                    </h5>
+                    <p className="mt-1 text-sm font-normal text-gray-600">
+                      {course.attributes.description}
+                    </p>
                   </div>
-                  <div class="flex items-center justify-between rounded-t-3xl p-3 w-full">
+                </div>
+                <div className="mt-1 flex items-center justify-center text-navy-700 dark:text-white">
+                  <div>
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 320 512"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path>
+                    </svg>
                   </div>
-                  <div class="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700">
-                      <div class="flex items-center gap-3">
-                          <div class="flex h-16 w-16 items-center justify-center">
-                              <img
-                                  class="h-full w-full rounded-xl"
-                                  src="https://horizon-tailwind-react-corporate-7s21b54hb-horizon-ui.vercel.app/static/media/Nft1.0fea34cca5aed6cad72b.png"
-                                  alt=""
-                              />
-                          </div>
-                          <div class="flex flex-col">
-                              <h5 class="text-base font-bold text-navy-700 dark:text-white">
-                                  Colorful Heaven
-                              </h5>
-                              <p class="mt-1 text-sm font-normal text-gray-600">
-                                  Mark Benjamin
-                              </p>
-                          </div>
-                      </div> 
-                      <div class="mt-1 flex items-center justify-center text-navy-700 dark:text-white">
-                          <div>
-                              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
-                          </div>
-                          <div class="ml-1 flex items-center text-sm font-bold text-navy-700 dark:text-white">
-                              <p>   </p>
-                              3299<p class="ml-1">Bath</p>
-                          </div>
-                          <div class="ml-2 flex items-center text-sm font-normal text-gray-600 dark:text-white">
-                              <p>7d</p>
-                              <p class="ml-1">ago</p>
-                          </div>
-                      </div>
+                  <div className="ml-1 flex items-center text-sm font-bold text-navy-700 dark:text-white">
+                    {course.attributes.price}
+                    <p className="ml-1">Bath</p>
                   </div>
-              </div>  
-              
+                  <div className="ml-2 flex items-center text-sm font-normal text-gray-600 dark:text-white">
+                    <p>5d</p>
+                    <p className="ml-1">ago</p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-      </body>
-    </html>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default HistoryPage;
