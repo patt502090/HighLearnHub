69d4926f90caacfbdfbf54fa1a768805858d70ad
ธุวานนท์ สงธนู <6610110134@psu.ac.th>
2024-02-26 18:05:40 +0700

    Modify edit button

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -83,7 +83,30 @@ function ProfilePage() {
       <div
         className="h-screen flex flex-col items-center justify-items-center pt-24 w-80 sm:w-full mx-auto"
       >
-        <div className="border border-gray-300 shadow-lg rounded-lg bg-white bg-opacity-90 p-8 w-full sm:w-5/6 md:w-3/4 lg:w-[80%]">
+        <div className="border border-gray-300 shadow-lg rounded-lg bg-white bg-opacity-90 p-8 w-full sm:w-5/6 md:w-3/4 lg:w-2/3 relative">
+          <button
+            onClick={handleEditClick}
+            className="text-black-500 hover:underline focus:outline-none focus:ring-2 focus:ring-black-600 focus:ring-opacity-20 absolute top-0 right-0 mr-4 mt-4"
+          >
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 512 512"
+              height="2em"
+              width="2em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="32"
+                d="M262.29 192.31a64 64 0 1057.4 57.4 64.13 64.13 0 00-57.4-57.4zM416.39 256a154.34 154.34 0 01-1.53 20.79l45.21 35.46a10.81 10.81 0 012.45 13.75l-42.77 74a10.81 10.81 0 01-13.14 4.59l-44.9-18.08a16.11 16.11 0 00-15.17 1.75A164.48 164.48 0 01325 400.8a15.94 15.94 0 00-8.82 12.14l-6.73 47.89a11.08 11.08 0 01-10.68 9.17h-85.54a11.11 11.11 0 01-10.69-8.87l-6.72-47.82a16.07 16.07 0 00-9-12.22 155.3 155.3 0 01-21.46-12.57 16 16 0 00-15.11-1.71l-44.89 18.07a10.81 10.81 0 01-13.14-4.58l-42.77-74a10.8 10.8 0 012.45-13.75l38.21-30a16.05 16.05 0 006-14.08c-.36-4.17-.58-8.33-.58-12.5s.21-8.27.58-12.35a16 16 0 00-6.07-13.94l-38.19-30A10.81 10.81 0 0149.48 186l42.77-74a10.81 10.81 0 0113.14-4.59l44.9 18.08a16.11 16.11 0 0015.17-1.75A164.48 164.48 0 01187 111.2a15.94 15.94 0 008.82-12.14l6.73-47.89A11.08 11.08 0 01213.23 42h85.54a11.11 11.11 0 0110.69 8.87l6.72 47.82a16.07 16.07 0 009 12.22 155.3 155.3 0 0121.46 12.57 16 16 0 0015.11 1.71l44.89-18.07a10.81 10.81 0 0113.14 4.58l42.77 74a10.8 10.8 0 01-2.45 13.75l-38.21 30a16.05 16.05 0 00-6.05 14.08c.33 4.14.55 8.3.55 12.47z"
+              ></path>
+            </svg>
+          </button>
+
           <div className="flex flex-col items-center mx-auto">
             <div className="h-40 w-40 lg:h-52 lg:w-52 overflow-hidden rounded-full mb-4 relative">
               <img
@@ -236,12 +259,6 @@ function ProfilePage() {
 
           <div className="mt-8">
             <h2 className="text-lg font-bold">ข้อมูลส่วนตัว</h2>
-            <button
-                onClick={handleEditClick}
-                className="text-red-500 hover:underline focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
-              >
-                แก้ไขข้อมูลส่วนตัว
-              </button>
             <div className="border-b border-gray-400 mt-4"></div>
             <div className="mt-4">
               <p className="text-base text-gray-700">
@@ -255,10 +272,6 @@ function ProfilePage() {
               <p className="text-base text-gray-700">
                 <span className="font-bold">อีเมล: </span>
                 {userData && `${userData.email}`}
-              </p>
-              <p className="text-base text-gray-700">
-                <span className="font-bold">เบอร์โทรศัพท์: </span>
-                {userData && `${userData.phonenum}`}
               </p>
             </div>
           </div>
