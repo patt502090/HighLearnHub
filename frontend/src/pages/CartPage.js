cc6fce252b92d251051a691e789f4165ad059f92
Phodcharaphon <patt502090@hotmail.com>
2024-02-28 01:42:56 +0700

    Update

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -19,9 +19,8 @@ export default function CartPage() {
             "/users/me?populate[bookings][filters][status][$eq]=cart&populate[bookings][populate][course][populate]=image"
         );
         console.log(response);
-        
+
         setCoursebooked(response.data.bookings);
-        
       } catch (error) {
         console.error("Error fetching Data:", error);
       }
@@ -40,107 +39,132 @@ export default function CartPage() {
 
   const deleteCourseBooked = async (id) => {
     try {
-      console.log("id =", id)
-      await ax.delete(
-        conf.apiUrlPrefix +
-          `/bookings/${id}`
-      );
+      console.log("id =", id);
+      await ax.delete(conf.apiUrlPrefix + `/bookings/${id}`);
 
-      setCoursebooked(coursebooked.filter(course => course.id !== id));
+      setCoursebooked(coursebooked.filter((course) => course.id !== id));
     } catch (error) {
       console.error("Error fetching Data:", error);
     }
   };
 
   console.log(coursebooked);
-  
+
   return (
     <>
-      <div className="bg-fixed" style={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
 
-        <Navbar />
-        <Progessbar></Progessbar>
-        <Helmet>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>ตะกร้าของฉัน</title>
-        </Helmet>
-        <div className="flex flex-col items-center mt-6">
-          <h className="text-xl font-bold">ตะกร้าของฉัน</h>
-          <div className="flex flex-col items-center mt-6">
-            <section className="py-17 bg-gray-100 font-poppins dark:bg-gray-700">
-              <div className="px-4 py-6 mx-auto max-w-7xl lg:py-4 md:px-6">
-                <div>
-                  <div className="p-6 mb-8 border bg-gray-50 dark:bg-gray-800 border-gray-800">
-                    <h1>สินค้าทั้งหมด {coursebooked.length} ชิ้น </h1>
-                    <div className="py-4 mb-10 border-t border-b border-gray-200 dark:border-gray-700">
-                      {coursebooked.length > 0 ? (
-                        coursebooked.map((item) => (
-                          <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" key={item.id}
-                            style={{ minWidth: "300px", maxWidth: "600px" }}
-                          >
-                            <img
-                              className="object-cover w-full rounded-t-lg h-96 max-lg:h-[11.5em] md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                              src={"http://localhost:1337" + item.course.image.url}
-                              alt=""
-                            />
-                            <div className="flex flex-col justify-between p-4 relative w-full">
-                              <button
-                                type="button"
-                                className="absolute top-0 right-0 mt-2 mr-2 bg-red-500 rounded-full p-2 inline-flex items-center text-white hover:text-white-500 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                                style={{ width: "30px", height: "30px" }}
-                                onClick={() => deleteCourseBooked(item.id)}
-                              >
-                                <IoTrashOutline />
-                              </button>
-                              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                {item.course.title}
-                              </h5>
-                              <p className="max-lg:hidden">{item.course.description}</p>
-                              <hr className="mt-6 " />
-                              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-between">
-                                <span>ราคา {item.course.price} บาท,</span>
-                                <span>x1</span>
+      <div className="sm:h-screen">
+        <div className="background-image">
+         <Navbar />
+          <div className="w-5/6 sm:w-6/12 mx-auto">
+            <div className="mr-4">
+              <Progessbar></Progessbar>
 
-                              </p>
+              <title>ตะกร้าของฉัน</title>
+              <div className="flex flex-col items-center ">
+                <h className="text-xl font-bold mt-5">ตะกร้าของฉัน</h>
+                <div className="flex flex-col items-center mt-6">
+                  <section
+                    className={
+                      coursebooked.length > 0
+                        ? "py-3 sm:py-15 w-6/7 sm:w-full bg-gray-100 font-poppins dark:bg-gray-700"
+                        : "py-3 sm:py-15 w-5/12 sm:w-full bg-gray-100 font-poppins dark:bg-gray-700"
+                    }
+                  >
+                    <div className="px-4 py-4 sm:py-6 mx-auto max-w-7xl lg:py-4 md:px-full">
+                      <div>
+                        <div className="p-5 sm:p-6 mb-2sm:mb-8 border bg-gray-50 dark:bg-gray-800 border-gray-800">
+                          <h1>สินค้าทั้งหมด {coursebooked.length} ชิ้น </h1>
+                          <div className="py-4 mb-5 sm:mb-10  border-t border-b border-gray-200 dark:border-gray-700">
+                            {coursebooked.length > 0 ? (
+                              coursebooked.map((item) => (
+                                <div
+                                  className="flex flex-col gap-1 mb-4 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+                                  key={item.id}
+                                >
+                                  <img
+                                    className="object-cover w-full sm:w-4/12 h-1/2 sm:h-auto rounded-t-lg md:rounded-none md:rounded-s-lg"
+                                    src={
+                                      "http://localhost:1337" +
+                                      item.course.image.url
+                                    }
+                                    alt=""
+                                  />
+                                  <div className="flex flex-col justify-between p-4 relative w-full">
+                                    <button
+                                      type="button"
+                                      className="absolute top-0 right-0 mt-2 mr-2 bg-red-500 rounded-full p-2 inline-flex items-center text-white hover:text-white-500 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                                      style={{ width: "30px", height: "30px" }}
+                                      onClick={() =>
+                                        deleteCourseBooked(item.id)
+                                      }
+                                    >
+                                      <IoTrashOutline />
+                                    </button>
+                                    <h5 className="mb-2 text-md sm:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                      {item.course.title}
+                                    </h5>
+                                    <p className="max-lg:hidden">
+                                      {item.course.description}
+                                    </p>
+                                    <hr className="mt-6 " />
+                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-between">
+                                      <span>ราคา {item.course.price} บาท,</span>
+                                      <span>x1</span>
+                                    </p>
+                                  </div>
+                                </div>
+                              ))
+                            ) : (
+                              <div>
+                                <p
+                                  style={{
+                                    fontSize: "100px",
+                                    visibility: "hidden",
+                                  }}
+                                >
+                                  aasdasdasac
+                                </p>
+                                <p
+                                  className="text-center"
+                                  style={{
+                                    fontSize: "20px",
+                                    fontFamily: "sans-serif",
+                                    fontWeight: "bold",
+                                  }}
+                                >
+                                  ไม่มีสินค้า
+                                </p>
+                                <div className="flex flex-col items-center"></div>
+                              </div>
+                            )}
+                          </div>
+
+                          <div className="flex flex-wrap justify-between">
+                            <div className="w-full px-4 mb-4 lg:w-1/2">
+                              ราคาทั้งหมด : {calculateTotalPrice()}
                             </div>
                           </div>
-                        ))
-                      ) : (
-                        
-                        <div>
-                        <p style={{ fontSize: "100px", visibility: "hidden" }}>aasdasdasac</p>
-                        <p className="text-center" style={{ fontSize: "20px", fontFamily: "sans-serif", fontWeight: "bold" }}>ไม่มีสินค้า</p>
-                        <div className="flex flex-col items-center">
-                        
                         </div>
-                        </div>
-                      )}
-                    </div>
-
-                    <div className="flex flex-wrap justify-between">
-                      <div className="w-full px-4 mb-4 lg:w-1/2">
-                        ราคาทั้งหมด : {calculateTotalPrice()}
                       </div>
                     </div>
+                  </section>
+                  <div className="flex items-center justify-between ">
+                    <Link Link to="/payment">
+                      <button
+                        className="block w-full py-5 px-11 font-bold text-center text-gray-100 uppercase bg-green-400 rounded-md hover:bg-blue-600"
+                        style={{ marginTop: "20px", marginBottom: "20px" }}
+                      >
+                        ชำระเงิน
+                      </button>
+                    </Link>
                   </div>
+                  <div></div>
                 </div>
               </div>
-            </section>
-            <div className="flex items-center justify-between ">
-              <Link Link to="/payment">
-                <button className="block w-full py-5 px-11 font-bold text-center text-gray-100 uppercase bg-green-400 rounded-md hover:bg-blue-600"
-                  style={{ marginTop: "20px", marginBottom: "20px" }}
-                >
-                  ชำระเงิน
-                </button>
-              </Link>
-            </div>
-            <div >
             </div>
           </div>
         </div>
--- frontend/src/pages/HistoryPage.js
+++ frontend/src/pages/HistoryPage.js
@@ -22,7 +22,7 @@ export default function HistoryPage() {
           (item) => item.status !== "cart"
         );
         setCoursebooked(filterDatas);
-        console.log(filterDatas)
+        console.log(filterDatas);
       } catch (error) {
         console.error("Error fetching Data:", error);
       }
@@ -32,16 +32,17 @@ export default function HistoryPage() {
   }, []);
 
   const calculateDaysAgo = (timestamp) => {
-    const oneDay = 24 * 60 * 60 * 1000; 
+    const oneDay = 24 * 60 * 60 * 1000;
     const currentDate = new Date();
     const paymentDate = new Date(timestamp);
     const diffDays = Math.round(Math.abs((currentDate - paymentDate) / oneDay));
     return diffDays;
   };
+
   return (
     <>
-    <div className="background-image">
       <Navbar />
+      <div className="background-image py-20">
         <head>
           <link
             rel="stylesheet"
@@ -49,46 +50,46 @@ export default function HistoryPage() {
           />
         </head>
         <Helmet>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <title>ประวัติการสั่งซื้อ</title>
         </Helmet>
-        <div className="h-screen flex flex-col justify-center items-center h-[60vh] pt-4">
-          <div className="relative flex flex-col items-center rounded-[10px] border-[1px] border-gray-200 w-[576px] mx-auto p-4 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
+        <div className="h-screen flex flex-col justify-center items-center pt-4">
+          <div className="relative flex flex-col items-center rounded-[10px] border-[1px] border-gray-200 w-5/6  h-full sm:w-[90%] md:w-[80%] lg:w-[70%] xl:w-[55%] mx-auto p-4 mt-20 sm:mt-0 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
             <div className="flex items-center justify-between rounded-t-3xl p-3 w-full">
               <div className="text-lg font-bold text-navy-700 dark:text-white">
                 History
               </div>
-              <button className="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-medium text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
+              <button className="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base md:text-sm font-medium text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
                 See all
               </button>
             </div>
             {coursebooked.map((item) => (
               <div
                 key={item.course.id}
-                className="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700"
+                className="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] md:py-4 transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700"
               >
                 <div className="flex items-center gap-3">
-                  <div className="flex h-16 w-16 items-center justify-center">
+                  <div className="flex h-auto  w-4/6 md:w-48 items-center justify-center">
                     <img
-                      className="object-cover w-full h-[100px] rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                      src={
-                        "http://localhost:1337" +
-                        item.course.image.url
-                      }
+                      className="object-cover w-full h-full md:h-auto md:w-full rounded-t-lg md:rounded-none md:rounded-s-lg"
+                      src={"http://localhost:1337" + item.course.image.url}
                       alt=""
                     />
                   </div>
                   <div className="flex flex-col">
-                    <h5 className="text-base font-bold text-navy-700 dark:text-white">
+                    <h5 className="text-sm sm:text-base font-bold text-navy-700 dark:text-white">
                       {item.course.title}
                     </h5>
-                    <p className="mt-1 text-sm font-normal text-gray-600">
+                    <p className="mt-1 text-xs sm:text-sm md:text-xs font-normal text-gray-600">
                       {item.course.description}
                     </p>
                   </div>
                 </div>
-                <div className="mt-1 flex items-center justify-center text-navy-700 dark:text-white">
-                  <div>
+                <div className="mt-1 flex items-center justify-between w-1/3 md:w-auto text-navy-700 dark:text-white">
+                  <div className="flex items-center">
                     <svg
                       stroke="currentColor"
                       fill="currentColor"
@@ -100,27 +101,24 @@ export default function HistoryPage() {
                     >
                       <path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path>
                     </svg>
+
+                    <div className="ml-1 flex items-center text-xs md:text-xs font-bold text-navy-700 dark:text-white">
+                      {item.course.price}
+                      <p className="ml-1">Bath</p>
+                    </div>
                   </div>
-                  <div className="ml-1 flex items-center text-sm font-bold text-navy-700 dark:text-white">
-                    {item.course.price}
-                    <p className="ml-1">Bath</p>
-                  </div>
-                  <div className="ml-2 flex items-center text-sm font-normal text-gray-600 dark:text-white">
+                  <div className="flex items-center text-xs md:text-xs font-normal sm:ml-4 text-gray-600 dark:text-white">
                     <p>{calculateDaysAgo(item.createdAt)}d ago</p>
                   </div>
-
-                  {item.status === "success"?(
-                  <div className="ml-3 flex items-center text-sm font-normal text-green-600">
+                  {item.status === "success" ? (
+                    <div className="flex items-center text-xs md:text-xs font-normal  sm:ml-4 text-green-600">
                       {item.status}
-
-                  </div>):(
-                     <div className="ml-3 flex items-center text-sm font-normal font-normal text-orange-600">
-                     in {item.status}
-
-                 </div>
-
+                    </div>
+                  ) : (
+                    <div className="flex items-center text-xs md:text-xs font-normal  sm:ml-4 text-orange-600">
+                      in {item.status}
+                    </div>
                   )}
-
                 </div>
               </div>
             ))}
