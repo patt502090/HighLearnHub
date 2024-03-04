ebea52a46e8720bf1b9da9e5038c4407a8d33672
Jatupat <kooseng2547@gmail.com>
2024-03-05 05:09:25 +0700

    chanceiconandeffect

--- frontend/src/components/Helper.js
+++ frontend/src/components/Helper.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import myImage from './main.png';
+
+
 
 const FloatingButton = () => {
   const [isFormVisible, setIsFormVisible] = useState(false);
   const [isopen, setopen] = useState(false);
-  const [formData, setFormData] = useState();
+
 
   const handleClick = () => {
     setopen(!isopen);
@@ -23,34 +26,17 @@ const FloatingButton = () => {
             className="w-50 h-50 mr-5"
           />
         ) : (
+          <div>
           <img
-            src="https://img.icons8.com/office/40/help.png" 
+            src={myImage}
             alt="help"
-            className="w-50 h-50 mr-5"
+            className="w-10 h-10 mr-5"
           />
+          </div>
         )}
       </button>
       {isFormVisible && (
-        <div className="absolute bottom-12 right-4 floating-button hover:scale-110">
-          <a href="tel:095-163-2351">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="48"
-              height="48"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="#000"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Zm3.232-8.639 1.544 1.548c.307.309.297.82-.02 1.14l-.442.432-.01-.009a2.606 2.606 0 0 1-1.274.52c-.1.011-2.435.23-5.333-2.674-2.097-2.101-2.87-3.65-2.665-5.344.023-.212.075-.422.16-.643.09-.232.212-.448.358-.634l-.012-.012.435-.439c.319-.32.829-.329 1.135-.021l1.545 1.548c.307.308.298.818-.02 1.138l-.258.257-.522.523a10.386 10.386 0 0 1 .084.15l.001.003c.273.493.647 1.167 1.355 1.876.708.71 1.38 1.084 1.872 1.357l.153.086.778-.78c.319-.32.828-.329 1.136-.021Z"
-              />
-            </svg>
-          </a>
-        </div>
-      )}
-      {isFormVisible && (
-        <div className="absolute bottom-24 right-4 floating-button hover:scale-110">
+        <div className="absolute bottom-14 right-4 floating-button hover:scale-110">
           <a
             href="https://line.me/R/ti/p/@210tmlid?oat_content=url"
             target="_blank"
@@ -77,7 +63,7 @@ const FloatingButton = () => {
         </div>
       )}
       {isFormVisible && (
-        <div className="absolute bottom-36 right-5 floating-button hover:scale-110">
+        <div className="absolute bottom-28 right-5 floating-button hover:scale-110">
           <a
             href="https://www.facebook.com/people/Highlearnhub/61556519805077/"
             target="_blank"
