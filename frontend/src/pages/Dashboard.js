8250f8f7da4db6f4f6d198e8601caea2601c488f
ธุวานนท์ สงธนู <6610110134@psu.ac.th>
2024-03-05 11:24:03 +0700

    update dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -14,17 +14,51 @@ function Dashboard() {
   const currentDate = new Date();
   const formattedDate = currentDate.toISOString();
   const [todayLogin, setTodayLogin] = useState();
-
+  const [loading, setLoading] = useState(false);
+  const [cData, setcData] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [profileId, setProfileId] = useState(null);
   // const calculateTodayLogin = () => {
   //   const cal = totalUsers?.filters((i) =>
 
   //   )
-  //   console.log("🚀 ~ calculateTodayLogin ~ cal:", cal)  
+  //   console.log("🚀 ~ calculateTodayLogin ~ cal:", cal)
   // }
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        setLoading(true);
+        const response = await ax.get(
+          `${conf.apiUrlPrefix}/users?populate[bookings][populate]=course&populate=image`
+        );
+        setcData(response.data);
+        setLoading(false);
+      };
+      fetchData();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const topCustomers = cData
+    .sort((a, b) => {
+      const totalAmountA = a.bookings.reduce(
+        (total, booking) => total + (booking.course?.price || 0),
+        0
+      );
+      const totalAmountB = b.bookings.reduce(
+        (total, booking) => total + (booking.course?.price || 0),
+        0
+      );
+      return totalAmountB - totalAmountA;
+    })
+    .slice(0, 5);
 
   const findTotalUsers = async () => {
     try {
-      const fetchUsers = await ax.get(`${conf.apiUrlPrefix}/users?populate=login_streak`)
+      const fetchUsers = await ax.get(
+        `${conf.apiUrlPrefix}/users?populate=login_streak`
+      );
       const filteredUsers = fetchUsers.data.filter((user) => {
         if (user.login_streak?.lastLogin) {
           const lastLoginDate = user.login_streak.lastLogin.slice(0, 10);
@@ -33,15 +67,15 @@ function Dashboard() {
         return false;
       });
       setTotalUsers(fetchUsers.data);
-      setTodayLogin(filteredUsers)
+      setTodayLogin(filteredUsers);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
     findTotalUsers();
-  }, [])
+  }, []);
 
   // useEffect(() => {
   //   calculateTodayLogin();
@@ -183,6 +217,46 @@ function Dashboard() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const onlineSellingResponse = await ax.get(
+          `${conf.apiUrlPrefix}/courses?populate=image&filters[study_type][$eq]=Online&sort=amount:desc&pagination[pageSize]=10&populate=videos`
+        );
+        const onlineSellingData = onlineSellingResponse.data.data.map(
+          (course) => {
+            const totalDurationSeconds = course.attributes.videos.data.reduce(
+              (totalDuration, video) =>
+                totalDuration + video.attributes.duration,
+              0
+            );
+
+            const minutes = Math.floor(totalDurationSeconds / 60);
+            const seconds = Math.floor(totalDurationSeconds % 60);
+
+            return {
+              id: course.id,
+              title: course.attributes.title,
+              price: course.attributes.price,
+              amount: course.attributes.amount,
+              description: course.attributes.description,
+              image:
+                "http://localhost:1337" +
+                course.attributes.image.data.attributes.url,
+              duration: { minutes, seconds },
+            };
+          }
+        );
+
+        setOnlineSelling(onlineSellingData);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <ContextProvider>
@@ -212,21 +286,38 @@ function Dashboard() {
               </svg>
               <h3 className="text-lg font-bold mb-2">ผู้ใช้ในระบบ</h3>
               <div className="flex justify-between w-full">
-                <p className="text-center mx-auto font-medium">{totalUsers?.length} คน</p>
+                <p className="text-center mx-auto font-medium">
+                  {totalUsers?.length} คน
+                </p>
               </div>
             </div>
             <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center justify-between">
-              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4 6-9 6s-9-4.8-9-6c0-1.2 4-6 9-6s9 4.8 9 6Z" />
-                <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              <svg
+                class="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-width="2"
+                  d="M21 12c0 1.2-4 6-9 6s-9-4.8-9-6c0-1.2 4-6 9-6s9 4.8 9 6Z"
+                />
+                <path
+                  stroke="currentColor"
+                  stroke-width="2"
+                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
               </svg>
               <h3 className="text-lg font-semibold mb-2">เข้าสู่ระบบวันนี้</h3>
               <div className="flex justify-between w-full">
                 <p className="text-gray-700 ml-2">{todayLogin?.length} คน</p>
-                {todayLogin?.length / totalUsers?.length * 100 >= 50
-                  ?
+                {(todayLogin?.length / totalUsers?.length) * 100 >= 50 ? (
                   <>
-                    <p className="text-green-500 mr-2">{todayLogin?.length / totalUsers?.length * 100}%</p>
+                    <p className="text-green-500 mr-2">
+                      {(todayLogin?.length / totalUsers?.length) * 100}%
+                    </p>
                     <svg
                       class="w-6 h-6 text-green-500 dark:text-white"
                       aria-hidden="true"
@@ -243,9 +334,11 @@ function Dashboard() {
                       />
                     </svg>
                   </>
-                  :
+                ) : (
                   <>
-                    <p className="text-red-500 mr-2">{todayLogin?.length / totalUsers?.length * 100}%</p>
+                    <p className="text-red-500 mr-2">
+                      {(todayLogin?.length / totalUsers?.length) * 100}%
+                    </p>
                     <svg
                       class="w-6 h-6 text-red-500 dark:text-white"
                       aria-hidden="true"
@@ -261,7 +354,8 @@ function Dashboard() {
                         d="M12 19V5m0 14-4-4m4 4 4-4"
                       />
                     </svg>
-                  </>}
+                  </>
+                )}
               </div>
             </div>
             <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center justify-between">
@@ -320,7 +414,7 @@ function Dashboard() {
               </svg>
               <h3 className="text-lg font-semibold mb-2">Total Products</h3>
               <div className="flex justify-between w-full">
-                <p className="text-gray-700 ml-2">1182</p>
+                <p className="text-gray-700 ml-2">1142</p>
                 <p className="text-red-500 mr-2">2.59%</p>
                 <svg
                   class="w-6 h-6 text-red-500 dark:text-white"
@@ -395,32 +489,114 @@ function Dashboard() {
               </div>
             </div>
           </div>
-          <div
-            className="bg-white p-6 rounded-lg shadow-lg w-1/2"
-            style={{ marginTop: "20px" }}
-          >
-            <h3 className="text-lg font-semibold mb-2">Top Products</h3>
-            <div className="grid grid-cols-1 gap-4">
-              <div className="border p-4 grid grid-cols-4 items-center">
-                <div className="text-center font-semibold">Product Name</div>
-                <div className="text-center font-semibold" style={{ marginLeft: "235px" }}>Price</div>
-                <div className="text-center font-semibold" style={{ marginLeft: "235px" }}>Sold</div>
-              </div>
-              {onlineSelling?.map((course) => (
-                <div
-                  key={course.id}
-                  className="border p-4 grid grid-cols-4 items-center"
-                >
-                  <img
-                    src={course.image}
-                    alt={course.title}
-                    className="w-40 h-20 object-cover mr-4"
-                  />
-                  <h4 className="font-semibold mb-1">{course.title}</h4>
-                  <p className="text-gray-700 text-center">${course.price}</p>
-                  <div className="text-center">{course.amount}</div>
+          <div class="flex">
+            <div
+              className="bg-white p-6 rounded-lg shadow-lg w-1/2"
+              style={{ marginTop: "20px" }}
+            >
+              <h3 className="text-lg font-semibold mb-2">Top Products</h3>
+              <div className="grid grid-cols-1 gap-4">
+                <div className="border p-4 grid grid-cols-4 items-center">
+                  <div className="text-center font-semibold">Product Name</div>
+                  <div
+                    className="text-center font-semibold"
+                    style={{ marginLeft: "235px" }}
+                  >
+                    Price
+                  </div>
+                  <div
+                    className="text-center font-semibold"
+                    style={{ marginLeft: "235px" }}
+                  >
+                    Sold
+                  </div>
                 </div>
-              ))}
+                {onlineSelling?.map((course) => (
+                  <div
+                    key={course.id}
+                    className="border p-4 grid grid-cols-4 items-center"
+                  >
+                    <img
+                      src={course.image}
+                      alt={course.title}
+                      className="w-40 h-20 object-cover mr-4"
+                    />
+                    <h4 className="font-semibold mb-1">{course.title}</h4>
+                    <p className="text-gray-700 text-center">${course.price}</p>
+                    <div className="text-center">{course.amount}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="overflow-x-auto w-1/2 h-full" style={{marginTop:"20px",marginLeft:"20px"}}>
+              <table className="w-full">
+                <thead>
+                  <tr className="bg-gray-900 text-left text-xs font-semibold uppercase tracking-widest text-white">
+                    <th className="px-5 py-3 text-base">ชื่อผู้ใช้</th>
+                    <th className="max-lg:hidden px-5 py-3 text-base">
+                      ชื่อ-สกุล
+                    </th>
+                    <th className="px-5 py-3 text-base">อีเมล</th>
+                    <th className="max-lg:hidden px-5 py-3 text-base">
+                      ยอดรวม
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="text-gray-700">
+                  {topCustomers.map((item, index) => (
+                    <tr key={index} className="">
+                      <td
+                        onClick={() => [
+                          setOpenModal(true),
+                          setProfileId(item.id),
+                        ]}
+                        className="flex items-center border-b border-gray-200 bg-white cursor-pointer hover:bg-slate-100 px-5 py-5 text-sm"
+                      >
+                        <div className="h-8 w-8 mr-2 flex-shrink-0">
+                          {item.image ? (
+                            <img
+                              className="object-cover w-8 h-8 rounded-full"
+                              src={`${conf.urlPrefix}${item.image.url}`}
+                              alt=""
+                            />
+                          ) : (
+                            <svg
+                              className="h-full w-full rounded-full text-gray-600 dark:text-white"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="currentColor"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M12 20a8 8 0 0 1-5-1.8v-.6c0-1.8 1.5-3.3 3.3-3.3h3.4c1.8 0 3.3 1.5 3.3 3.3v.6a8 8 0 0 1-5 1.8ZM2 12a10 10 0 1 1 10 10A10 10 0 0 1 2 12Zm10-5a3.3 3.3 0 0 0-3.3 3.3c0 1.7 1.5 3.2 3.3 3.2 1.8 0 3.3-1.5 3.3-3.3C15.3 8.6 13.8 7 12 7Z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                          )}
+                        </div>
+                        <p className="whitespace-no-wrap">{item.username}</p>
+                      </td>
+                      <td className="max-lg:hidden border-b border-gray-200 bg-white  px-5 py-5 text-sm">
+                        <p className="whitespace-no-wrap">{`${item.first_name} ${item.last_name}`}</p>
+                      </td>
+                      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                        <p className="whitespace-no-wrap">{item.email}</p>
+                      </td>
+                      <td className="max-lg:hidden border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                        <p className="whitespace-no-wrap">
+                          {item.bookings.reduce(
+                            (total, booking) =>
+                              total + (booking.course?.price || 0),
+                            0
+                          )}{" "}
+                          บาท
+                        </p>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
